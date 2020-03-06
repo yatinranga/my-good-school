6735a22beaac51541478594f5e4f23822620d129
yashchaturvedi <chaturvedi.yash97@gmail.com>
2020-03-06 23:29:20 +0530

    Teacher Signup Validation

--- mgs/src/main/angular/src/app/services/student.service.ts
+++ mgs/src/main/angular/src/app/services/student.service.ts
@@ -14,6 +14,7 @@ export class StudentService {
     const url = "/student/signUp"
     return this.http.post(url, data);
   }
+  
   // Single teacher signup
   uploadTeacherDetails(data) {
     const url = "/teacher/signUp"
