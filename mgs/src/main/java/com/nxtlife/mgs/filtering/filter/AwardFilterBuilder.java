786bad8dc7e3b28cbd347c2fd48e177b0c882331
Ajay Garg <ajaykumargarg01@gmail.com>
2020-03-07 18:32:14 +0530

    Filter on awards and achievement

--- mgs/src/main/java/com/nxtlife/mgs/filtering/filter/AwardFilterBuilder.java
+++ mgs/src/main/java/com/nxtlife/mgs/filtering/filter/AwardFilterBuilder.java
@@ -1,33 +1,58 @@
 package com.nxtlife.mgs.filtering.filter;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
-import com.nxtlife.mgs.entity.school.QAwardActivityPerformed;
+import com.nxtlife.mgs.entity.activity.Activity;
+import com.nxtlife.mgs.entity.school.QAward;
+import com.nxtlife.mgs.enums.AwardStatus;
 import com.querydsl.core.types.Predicate;
 
 @Component
-public class AwardFilterBuilder implements FilterBuilder<AwardFilter> {
+public class AwardFilterBuilder {
 
-	private final QAwardActivityPerformed AwardActivityPerformed = QAwardActivityPerformed.awardActivityPerformed;
+	private final QAward qAward = QAward.award;
 
-	@Override
 	public Predicate build(AwardFilter filter) {
-		return new OptionalBooleanBuilder(AwardActivityPerformed.isNotNull())
-				// .notEmptyAnd(AwardActivityPerformed.assignerCid::contains,
-				// filter.getAssignerId())
-				.notEmptyAnd(AwardActivityPerformed.activityPerformed.activity.fourS.stringValue()::containsIgnoreCase,
-						filter.getFourS())
-				.notEmptyAnd(AwardActivityPerformed.activityPerformed.activityStatus.stringValue()::containsIgnoreCase,
-						filter.getStatus())
-				.notEmptyAnd(AwardActivityPerformed.activityPerformed.activity.cid::contains, filter.getActivityId())
-				.notEmptyAnd(AwardActivityPerformed.activityPerformed.activity.focusAreas.any().cid::contains,
-						filter.getFocusAreaId())
-				.notEmptyAnd(AwardActivityPerformed.activityPerformed.activity.focusAreas.any().psdArea
-						.stringValue()::containsIgnoreCase, filter.getPsdArea())
-				// .notEmptyAnd(AwardActivityPerformed.dateOfReceipt.year().stringValue()::containsIgnoreCase,
-				// filter.getYear())
-				.build();
-		// return null;
+		return new OptionalBooleanBuilder(qAward.isNotNull())
+				.notEmptyAnd(qAward.activity.cid.stringValue()::containsIgnoreCase, filter.getActivityId())
+				.notEmptyAnd(qAward.dateOfReceipt.year().stringValue()::containsIgnoreCase, filter.getYear())
+				.notEmptyAnd(qAward.student.cid.stringValue()::containsIgnoreCase, filter.getStudentId())
+				.notEmptyAnd(qAward.teacher.cid.stringValue()::containsIgnoreCase, filter.getTeacherId())
+				.notEmptyAnd(qAward.activity.focusAreas.any().cid::contains, filter.getFocusAreaId())
+				.notEmptyAnd(qAward.activity.focusAreas.any().psdArea.stringValue()::containsIgnoreCase,
+						filter.getPsdArea())
+				.notEmptyAnd(qAward.activity.fourS.stringValue()::containsIgnoreCase, filter.getFourS()).build();
+	}
+
+	public Predicate build(AwardFilter filter, String studentId, AwardStatus status) {
+		return new OptionalBooleanBuilder(qAward.isNotNull())
+				.notEmptyAnd(qAward.activity.cid.stringValue()::containsIgnoreCase, filter.getActivityId())
+				.notEmptyAnd(qAward.dateOfReceipt.year().stringValue()::containsIgnoreCase, filter.getYear())
+				.notEmptyAnd(qAward.student.cid.stringValue()::containsIgnoreCase, filter.getStudentId())
+				.notEmptyAnd(qAward.teacher.cid.stringValue()::containsIgnoreCase, filter.getTeacherId())
+				.notEmptyAnd(qAward.activity.focusAreas.any().cid::contains, filter.getFocusAreaId())
+				.notEmptyAnd(qAward.activity.focusAreas.any().psdArea.stringValue()::containsIgnoreCase,
+						filter.getPsdArea())
+				.notEmptyAnd(qAward.student.cid.stringValue()::containsIgnoreCase, studentId)
+				.notEmptyAnd(qAward.active.stringValue()::containsIgnoreCase, "TRUE")
+				.notEmptyAnd(qAward.status.stringValue()::containsIgnoreCase, status.name())
+				.notEmptyAnd(qAward.activity.fourS.stringValue()::containsIgnoreCase, filter.getFourS()).build();
+	}
+
+	public Predicate build(AwardFilter filter, List<Activity> activities) {
+		return new OptionalBooleanBuilder(qAward.isNotNull())
+				.notEmptyAnd(qAward.activity.cid.stringValue()::containsIgnoreCase, filter.getActivityId())
+				.notEmptyAnd(qAward.dateOfReceipt.year().stringValue()::containsIgnoreCase, filter.getYear())
+				.notEmptyAnd(qAward.student.cid.stringValue()::eq, filter.getStudentId())
+				.notEmptyAnd(qAward.teacher.cid.stringValue()::eq, filter.getTeacherId())
+				.notEmptyAnd(qAward.activity.focusAreas.any().cid::contains, filter.getFocusAreaId())
+				.notEmptyAnd(qAward.activity.focusAreas.any().psdArea.stringValue()::containsIgnoreCase,
+						filter.getPsdArea())
+				.notEmptyAnd(qAward.activity::in, activities)
+				.notEmptyAnd(qAward.active.stringValue()::containsIgnoreCase, "TRUE")
+				.notEmptyAnd(qAward.activity.fourS.stringValue()::containsIgnoreCase, filter.getFourS()).build();
 	}
 
 }
--- mgs/src/main/java/com/nxtlife/mgs/jpa/AwardRepository.java
+++ mgs/src/main/java/com/nxtlife/mgs/jpa/AwardRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nxtlife.mgs.entity.activity.Activity;
 import com.nxtlife.mgs.entity.school.Award;
 import com.nxtlife.mgs.enums.AwardStatus;
+import com.querydsl.core.types.Predicate;
 
 public interface AwardRepository extends JpaRepository<Award, Long> {
 	
 	List<Award> findByStudentIdAndStatus(Long studentId, AwardStatus status);
 	
 	List<Award> findByActivity(List<Activity> activities);
+	
+	List<Award> findAll(Predicate predicate);
+	
+	Award findByCidAndActiveTrue(String cid);
 }
