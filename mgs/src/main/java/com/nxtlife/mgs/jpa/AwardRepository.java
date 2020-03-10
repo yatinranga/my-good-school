25d6c379ec018c3e67f781a48a0a5089e697058c
Ajay Garg <ajaykumargarg01@gmail.com>
2020-03-10 22:54:02 +0530

    Award fetch for teacher bug fixing

--- mgs/src/main/java/com/nxtlife/mgs/jpa/AwardRepository.java
+++ mgs/src/main/java/com/nxtlife/mgs/jpa/AwardRepository.java
@@ -3,19 +3,21 @@ package com.nxtlife.mgs.jpa;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 import com.nxtlife.mgs.entity.activity.Activity;
 import com.nxtlife.mgs.entity.school.Award;
 import com.nxtlife.mgs.enums.AwardStatus;
 import com.querydsl.core.types.Predicate;
 
-public interface AwardRepository extends JpaRepository<Award, Long> {
+public interface AwardRepository extends JpaRepository<Award, Long>, QueryDslPredicateExecutor<Award> {
+
+	public List<Award> findByStudentIdAndStatus(Long studentId, AwardStatus status);
+
+	public List<Award> findByActivityIn(List<Activity> activities);
+
+	public List<Award> findAll(Predicate predicate);
+
+	public Award findByCidAndActiveTrue(String cid);
 	
-	List<Award> findByStudentIdAndStatus(Long studentId, AwardStatus status);
-	
-	List<Award> findByActivity(List<Activity> activities);
-	
-	List<Award> findAll(Predicate predicate);
-	
-	Award findByCidAndActiveTrue(String cid);
 }
