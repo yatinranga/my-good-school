4ea569912f16b9cc7e27c25396dbaedebce9c3c3
yashchaturvedi <chaturvedi.yash97@gmail.com>
2020-03-12 10:54:57 +0530

    nav bar changes

--- mgs/src/main/angular/src/app/modules/teacher/teacher.module.ts
+++ mgs/src/main/angular/src/app/modules/teacher/teacher.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
         component: TeacherProfileComponent
       },
       {
-        path: 'activity',
+        path: 'activities',
         component: TeacherActivityComponent
       },
       {
